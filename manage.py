d2402410a774c1cac6228d862617feb3f2ce21eb
Take-Me1010 <take_ohmi@icloud.com>
2024-05-22 22:06:02 +0900

    🚧 add command manager

---
+++ manage.py
@@ -0,0 +1,81 @@
+"""
+ref: https://discord.com/developers/docs/interactions/application-commands#slash-commands
+"""
+
+import os
+import sys
+
+import requests
+
+# HACK
+sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
+import app.commands
+
+
+APP_ID = os.environ["APP_ID"]
+SERVER_ID = os.environ["SERVER_ID"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+
+def confirm():
+    print("ok? (y/n)")
+    if input() != "y":
+        return False
+    return True
+
+
+def register():
+    payload = [
+        command.toDict() for command in app.commands.DiscordCommands.get_commands()
+    ]
+
+    print("Try to register the following commands:\n")
+    for p in payload:
+        print(p)
+    print(f"\nTotal: {len(payload)}")
+
+    if not confirm():
+        return
+
+    # global commands
+    # url = f'https://discord.com/api/v10/applications/{APP_ID}/commands'
+
+    # server commands
+    url = (
+        f"https://discord.com/api/v10/applications/{APP_ID}/guilds/{SERVER_ID}/commands"
+    )
+
+    response = requests.put(
+        url, headers={"Authorization": f"Bot {BOT_TOKEN}"}, json=payload
+    )
+
+    print(response.json())
+
+
+def remove(name: str):
+    # global commands
+    # url = f'https://discord.com/api/v10/applications/{APP_ID}/commands/{name}'
+
+    # server commands
+    url = f"https://discord.com/api/v10/applications/{APP_ID}/guilds/{SERVER_ID}/commands/{name}"
+
+    response = requests.delete(url, headers={"Authorization": f"Bot {BOT_TOKEN}"})
+
+    print(response.json())
+
+
+def main(*args: str):
+    if "-Remove" in args and len(args) == 2:
+        remove(args[1])
+
+    else:
+        register()
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        main(*sys.argv[1:])
+    else:
+        main()
