4bf64b789ceb6e89f5688a840de554e81783e97f
Take-Me1010 <take_ohmi@icloud.com>
2024-05-23 22:59:46 +0900

    📝 add comments

--- app/helpers.py
+++ app/helpers.py
@@ -26,6 +26,26 @@ def toResponse(
 def registerCommand(
     name: str, description: str, options: tp.List[DiscordApplicationCommandOption] = []
 ):
+    """convert a function to a command and register it by calling `DiscordCommands.register( func )`
+
+    After decorating with this in command.py, you can access the function by `DiscordCommands.get(name)` as follows:
+    >>> from commands import DiscordCommands
+    >>> command = DiscordCommands.get(name)
+
+    Args:
+        name (str): name of the command
+        description (str): description of the command
+        options (tp.List[DiscordApplicationCommandOption], optional): options to determine its args. Defaults to [].
+
+    Example:
+    >>> @registerCommand("ping", "ping pong")
+    >>> def ping(body: DiscordCommandRequestBody) -> DiscordCommandResponseBody:
+    >>>     return {
+    >>>         "type": InteractionType.CHANNEL_MESSAGE_WITH_SOURCE,
+    >>>         "data": {"content": "pong"},
+    >>>     }
+    """
+
     def decorator(func: CommandHandler):
         c = Command.New(func, name, description, options)
         DiscordCommands.register(c)
